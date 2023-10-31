84586de43a305391c2a270e0d8d2dd97aba2da51
andy7560683 <34652047+andy7560683@users.noreply.github.com>
2023-10-31 13:58:16 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int t;
+  double pay;
+  scanf("%d", &t);
+  if(t <= 800){
+    pay = t*0.9;
+  }
+  else if(t > 800 && t <= 1500){
+    pay = t*0.9*0.9;
+  }
+  else if(t > 1500){
+    pay = t*0.9*0.79;
+  }
+
+  printf("%.1f", pay);
+}
 
