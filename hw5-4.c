afdc8846a8a0f48d4775f25acd94b9a2ace8d39c
andy7560683 <34652047+andy7560683@users.noreply.github.com>
2023-10-31 15:39:35 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int H, M;
+    scanf("%d %d", &H, &M);
+    while(1){
+        if(H >= 1 && H <= 12 && M >= 00 && M <=59){
+            break;
+        }
+    }
+
+    float angle_m, angle_h;
+    angle_m = 360*(M/60.0);
+    angle_h = 360*((H/12.0)+(M/720.0));
+    if(angle_m >= 180){
+        angle_m = angle_m - 180;
+    }
+    if(angle_h >= 180){
+        angle_h = angle_h - 180;
+    }
+
+    float angle;
+    if(angle_m > angle_h){
+        angle = angle_m - angle_h;
+    }
+    else if(angle_h > angle_m){
+        angle = angle_h - angle_m;
+    }
+    else{
+        angle = 0;
+    }
+
+    if((M < 30 && H > 6) || (M > 30 && H < 6)){
+      angle = (180 - angle);
+    }
+    printf("%.3f", angle);
+
+}
 
